f7549468044bec2d29cc27db266684d95484cf66
jmctheowl8 <john.cabrera@lc.cuny.edu>
2023-10-29 13:59:14 -0400

    add html to app.js

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 function App() {
-     return (
+    return (
         <div>
             <h1>Tic-React-Toe</h1>
+    
+            <h2>It's X's turn!</h2> 
+            
+            <div class="flex-container flex-column">
+                
+                <div class="flex-container flex-wrap" id="board">
+                    
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                
+                </div>
+                
+                <button id="reset-button">reset</button>
+            
+            </div>
         </div>
     )
- }
- ReactDOM.render(<App />, root)
+}
+ReactDOM.render(<App />, root)
